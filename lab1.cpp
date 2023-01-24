f513d4b4f423550301d0a59a8d452f5e4b901a29
Student <student@lab-240-014.cs.csubak.edu>
2023-01-24 09:23:28 -0800

    my first commit for lab-1

---
+++ lab1.cpp
@@ -0,0 +1,18 @@
+//Nathan Flessati
+//Lab1
+//jan24
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+
+    int a = rand() % 10 + 1;
+    cout << a << endl;
+    cout << "Hi World How Are You\n";    
+    
+    return 0;
+}
